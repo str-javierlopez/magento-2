fca9ac5b9004dd2e4c013de3870b6873991d58bb
str-javierlopez <javier_girela@stratesys-ts.com>
2020-09-29 19:30:52 +0200

    End points

---
+++ app/code/Hiberus/Lopez/Helper/Api.php
@@ -0,0 +1,135 @@
+<?php
+
+
+namespace Hiberus\Lopez\Helper;
+
+use Hiberus\Lopez\Api\Data\HiberusExamsInterface;
+use Hiberus\Lopez\Api\ExamManagementInterface;
+use Hiberus\Lopez\Api\HiberusExamsRepositoryInterface;
+use Hiberus\Lopez\Model\HiberusExamsFactory;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Framework\Exception\CouldNotDeleteException;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+/**
+ * Class Api
+ * @package Hiberus\Lopez\Helper
+ */
+class Api extends AbstractHelper
+{
+
+    /**
+     * @var ExamManagementInterface
+     */
+    private $_examManager;
+
+    /**
+     * @var HiberusExamsRepositoryInterface
+     */
+    private $_hiberusExamsRepository;
+
+    /**
+     * @var HiberusExamsFactory
+     */
+    private $_hiberusExamsFactory;
+
+    /**
+     * @var string
+     */
+    public $_exceptionsMessages;
+
+    /**
+     * Api constructor.
+     * @param Context $context
+     * @param ExamManagementInterface $examManagement
+     * @param HiberusExamsRepositoryInterface $hiberusExamsRepository
+     * @param HiberusExamsFactory $hiberusExamsFactory
+     */
+    public function __construct(
+        Context $context,
+        ExamManagementInterface $examManagement,
+        HiberusExamsRepositoryInterface $hiberusExamsRepository,
+        HiberusExamsFactory $hiberusExamsFactory
+    ) {
+        $this->_examManager            = $examManagement;
+        $this->_hiberusExamsRepository = $hiberusExamsRepository;
+        $this->_hiberusExamsFactory    = $hiberusExamsFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Retrieve a list of all students exams
+     * @return array
+     * @throws LocalizedException
+     */
+    public function getStudentsExamsList() : array
+    {
+        $searchResult = $this->_examManager->getList();
+
+        return $searchResult->getCollection()->getData();
+    }
+
+    /**
+     * Remove exam by id
+     * @param $idExam
+     * @return bool
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    public function removeStudentExamById($idExam) : bool
+    {
+        try {
+            $this->_hiberusExamsRepository->deleteByIdExam($idExam);
+        } catch (CouldNotDeleteException $exception) {
+            $this->_exceptionsMessages = $exception->getMessage();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Create a student exam
+     * @param $params
+     * @return bool
+     */
+    public function createStudentExam($params)
+    {
+        $hiberusExam = $this->createHiberusStudentExam($params);
+        return $this->saveHiberusStudentExam($hiberusExam);
+    }
+
+    /**
+     * Save an student exam
+     * @param HiberusExamsInterface $hiberusExams
+     * @return bool
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    private function saveHiberusStudentExam(HiberusExamsInterface $hiberusExams) : bool
+    {
+        try {
+            $this->_hiberusExamsRepository->save($hiberusExams);
+        } catch (CouldNotSaveException $exception) {
+            $this->_exceptionsMessages = $exception->getMessage();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Create a hiberus student exam object
+     * @param $params
+     * @return HiberusExamsInterface
+     */
+    private function createHiberusStudentExam($params) : HiberusExamsInterface
+    {
+        $hiberusExam = $this->_hiberusExamsFactory->create();
+        $hiberusExam->setFirstname($params['firstname']);
+        $hiberusExam->setLastname($params['lastname']);
+        $hiberusExam->setMark($params['mark']);
+        return $hiberusExam;
+    }
+}
---
+++ app/code/Hiberus/Lopez/Service/GetApiStudentsExams.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Hiberus\Lopez\Service;
+
+use Hiberus\Lopez\Api\GetApiStudentsExamsInterface;
+use Magento\Framework\Controller\Result\Json;
+use Magento\Framework\App\Request\Http;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Integration\Model\Oauth\Consumer;
+use Hiberus\Lopez\Helper\Api as HelperApi;
+
+/**
+ * Class GetApiStudentsExams
+ * @package Hiberus\Lopez\Service
+ */
+class GetApiStudentsExams implements GetApiStudentsExamsInterface
+{
+
+    /**
+     * @var Http
+     */
+    private $_request;
+
+    /**
+     * @var JsonFactory
+     */
+    private $_jsonFactory;
+
+    /**
+     * @var Consumer
+     */
+    private $_consumerApi;
+
+    /**
+     * @var HelperApi
+     */
+    private $_helperApi;
+
+    /**
+     * @var array
+     */
+    private $_requestParams = [];
+
+    /**
+     * GetApiStudentsExams constructor.
+     * @param Http $request
+     * @param JsonFactory $jsonFactory
+     * @param Consumer $consumerApi
+     * @param HelperApi $helper_api
+     */
+    public function __construct(
+        Http $request,
+        JsonFactory $jsonFactory,
+        Consumer $consumerApi,
+        HelperApi $helper_api
+    ) {
+        $this->_request      = $request;
+        $this->_jsonFactory  = $jsonFactory;
+        $this->_consumerApi  = $consumerApi;
+        $this->_helperApi    = $helper_api;
+    }
+
+    /**
+     * Validate if api key is valid
+     * @param $apiKey
+     * @return bool
+     */
+    private function validateCustomerApi($apiKey) : bool
+    {
+        $consumer = $this->_consumerApi->loadByKey($apiKey);
+
+        return $consumer->getId() !== null;
+    }
+
+    /**
+     * Validate if is posible remove an exam
+     * @return bool
+     */
+    private function canRemoveExam()
+    {
+        return isset($this->_requestParams['id_exam']);
+    }
+
+    /**
+     * Validate if is posible create an exam
+     * @return bool
+     */
+    private function canCreateExam()
+    {
+        return isset($this->_requestParams['firstname']) && isset($this->_requestParams['lastname'])
+            && isset($this->_requestParams['mark']);
+    }
+
+    /**
+     * Validate the request
+     * @return bool
+     */
+    private function validateRequest()
+    {
+        if (!$this->_request->isPost() && !$this->_request->isGet()) {
+            return false;
+        }
+
+        $this->_requestParams = $this->_request->getParams();
+
+        if (!isset($this->_requestParams['apiKey'])) {
+            return false;
+        }
+
+        return $this->validateCustomerApi($this->_requestParams['apiKey']);
+    }
+
+    /**
+     * List of Student exam Api action
+     * @return Json
+     * @throws LocalizedException
+     */
+    public function getExamsList(): string
+    {
+        $response = ['status' => false];
+        if (!$this->validateRequest()) {
+            return json_encode($response);
+        }
+
+        $studentsExams = $this->_helperApi->getStudentsExamsList();
+
+        $response = ['status' => true, 'exams' => $studentsExams];
+
+        return json_encode($response);
+    }
+
+    /**
+     * Remove Student exam Api action
+     * @return Json
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    public function removeExamById(): Json
+    {
+        $response = $this->_jsonFactory->create()->setData(['status' => false]);
+        if (!$this->validateRequest()) {
+            return $response;
+        }
+        if (!$this->canRemoveExam()) {
+            $response->setData(['status' => false, 'error' => __('Parameters missing')]);
+            return $response;
+        }
+
+        $idExam           = $this->_requestParams['id_exam'];
+        $isDeleted        = $this->_helperApi->removeStudentExamById($idExam);
+        $messageSuccess   = __('The exam with id %1 has been deleted.', $idExam);
+        $message          = $isDeleted ? $messageSuccess : $this->_helperApi->_exceptionsMessages;
+
+        $response->setData(['status' => true, 'message' => $message]);
+
+        return $response;
+    }
+
+    /**
+     * Add Student exam Api action
+     * @return Json
+     */
+    public function addStudentExam(): Json
+    {
+        $response = $this->_jsonFactory->create()->setData(['status' => false]);
+        if (!$this->validateRequest()) {
+            return $response;
+        }
+        if (!$this->canCreateExam()) {
+            return $response;
+        }
+
+        $this->_helperApi->createStudentExam($this->_requestParams);
+        return $response;
+    }
+
+}
